4a721dbdccfee0deedd601cdf5762826a9278a5d
liwenjing <levent_j@163.com>
2018-12-05 16:55:07 +0800

    更新主要流程说明

---
+++ app/src/main/java/com/levent_j/artifacthelper/modules/main/IMainCallback.java
@@ -0,0 +1,10 @@
+package com.levent_j.artifacthelper.modules.main;
+
+/*********************************************************************
+ * This file is part of Artifact project
+ * Created by levent_j on 2018/12/05.
+ * Copyright ￼ 2018 NetEase, Inc. - All Rights Reserved
+ *********************************************************************/
+public interface IMainCallback {
+
+}
---
+++ app/src/main/java/com/levent_j/artifacthelper/modules/main/MainPresenter.java
@@ -0,0 +1,20 @@
+package com.levent_j.artifacthelper.modules.main;
+
+import com.levent_j.artifacthelper.base.BasePresenter;
+
+/*********************************************************************
+ * This file is part of Artifact project
+ * Created by levent_j on 2018/12/05.
+ * Copyright ￼ 2018 NetEase, Inc. - All Rights Reserved
+ *********************************************************************/
+public class MainPresenter extends BasePresenter{
+
+
+
+    @Override
+    public void onDestory() {
+
+    }
+
+
+}
