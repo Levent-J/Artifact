0a350712dcd7216f1b078bdeb085e6bacad71765
liwenjing <levent_j@163.com>
2018-12-07 16:46:00 +0800

    加入了头条的屏幕适配方案

--- app/src/main/java/com/levent_j/artifacthelper/base/BaseActivity.java
+++ app/src/main/java/com/levent_j/artifacthelper/base/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.levent_j.artifacthelper.base;
 
+import android.content.ComponentCallbacks;
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 
 import com.trello.rxlifecycle2.components.RxActivity;
 
@@ -14,12 +18,17 @@ import java.util.ArrayList;
  *********************************************************************/
 public abstract class BaseActivity extends RxActivity {
 
+    private static float sNonDensity;
+    private static float sNonScaledDensity;
+    private static final int WIDTH_DESIGN = 360;
+
     private ArrayList<BasePresenter> mPresenters;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutId());
+        setUpDensity(this,GlobalData.app());
         initPresenter();
         initView();
         initData();
@@ -35,6 +44,44 @@ public abstract class BaseActivity extends RxActivity {
             }
             mPresenters.clear();
         }
+    }
+
+    /**
+     * 屏幕适配 采取了今日头条的方案
+     * @param activity
+     * @param context
+     */
+    private void setUpDensity(BaseActivity activity, final Context context) {
+        final DisplayMetrics appDisplayMetrics = context.getResources().getDisplayMetrics();
+        if (sNonDensity==0){
+            sNonDensity = appDisplayMetrics.density;
+            sNonScaledDensity = appDisplayMetrics.scaledDensity;
+            context.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if (newConfig!=null && newConfig.fontScale >0){
+                        sNonScaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+
+                }
+            });
+        }
+        final float targetDensity = appDisplayMetrics.widthPixels/WIDTH_DESIGN;//以设计图宽度适配
+        final float targetScaleDensity = targetDensity * (sNonScaledDensity/sNonDensity);
+        final int targetDensityDpi = (int) (160 * targetDensity);
+
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.scaledDensity = targetScaleDensity;
+        appDisplayMetrics.densityDpi = targetDensityDpi;
+
+        final DisplayMetrics activityMetrics = activity.getResources().getDisplayMetrics();
+        activityMetrics.density = targetDensity;
+        activityMetrics.scaledDensity = targetScaleDensity;
+        activityMetrics.densityDpi = targetDensityDpi;
     }
 
     protected abstract int setLayoutId();
