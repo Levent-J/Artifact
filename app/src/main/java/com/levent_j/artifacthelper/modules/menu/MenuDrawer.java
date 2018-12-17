ea7d0320522509f81ef44732cb8e274db41334fc
liwenjing <levent_j@163.com>
2018-12-17 20:21:10 +0800

    侧滑菜单栏UI完成

--- app/src/main/java/com/levent_j/artifacthelper/modules/menu/MenuDrawer.java
+++ app/src/main/java/com/levent_j/artifacthelper/modules/menu/MenuDrawer.java
@@ -2,6 +2,8 @@ package com.levent_j.artifacthelper.modules.menu;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.levent_j.artifacthelper.R;
@@ -11,15 +13,55 @@ import com.levent_j.artifacthelper.R;
  * Created by levent_j on 2018/12/16.
  * Copyright ￼ 2018 NetEase, Inc. - All Rights Reserved
  *********************************************************************/
-public class MenuDrawer extends RelativeLayout {
+public class MenuDrawer extends RelativeLayout implements View.OnClickListener {
+
+    private LinearLayout mCardList;
+    private LinearLayout mHostSet;
+    private LinearLayout mMatchSign;
+    private LinearLayout mSetting;
+    private LinearLayout mAbout;
+    private LinearLayout mParent;
+
+    private LinearLayout mCurrent;
+
     public MenuDrawer(Context context) {
         super(context);
     }
 
     public MenuDrawer(Context context, AttributeSet attrs) {
         super(context, attrs);
-        inflate(context, R.layout.view_menu_drawer,this);
+        inflate(context, R.layout.view_menu_drawer, this);
+
+        mCardList = findViewById(R.id.ll_menu_card_list);
+        mHostSet = findViewById(R.id.ll_menu_host_cardset);
+        mMatchSign = findViewById(R.id.ll_menu_match_sign);
+        mSetting = findViewById(R.id.ll_menu_setting);
+        mAbout = findViewById(R.id.ll_menu_about);
+        mParent = findViewById(R.id.ll_menu_parent);
+
+        mCardList.setOnClickListener(this);
+        mHostSet.setOnClickListener(this);
+        mMatchSign.setOnClickListener(this);
+        mSetting.setOnClickListener(this);
+        mAbout.setOnClickListener(this);
+        mParent.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_menu_card_list:
+                break;
+            case R.id.ll_menu_host_cardset:
+                break;
+            case R.id.ll_menu_match_sign:
+                break;
+            case R.id.ll_menu_setting:
+                break;
+            case R.id.ll_menu_about:
+                break;
+
+        }
+    }
 }
